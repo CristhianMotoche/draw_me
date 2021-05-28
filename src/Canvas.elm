9e223908799135b1183fdcedda9f00028a5d8724
CristhianMotoche <cristhian.motoche@gmail.com>
2021-05-27 21:47:03 -0500

    Control point click position

--- src/Canvas.elm
+++ src/Canvas.elm
@@ -1,7 +1,9 @@
 module Canvas exposing (..)
 
 import Browser
+import Tuple as T
 import Html as H
+import Html.Events.Extra.Mouse as M
 
 type alias Model =
   { currentPoint : Point
@@ -20,13 +22,16 @@ init = { currentPoint = { x = 0, y = 0 } }
 view : Model -> H.Html Msg
 view model =
   H.div
-    []
+    [ M.onDown <| \ev -> Paint { x = T.first ev.clientPos, y = T.second ev.clientPos } ]
     [ H.text <| "X" ++ String.fromFloat model.currentPoint.x
     , H.text <| "Y" ++ String.fromFloat model.currentPoint.y
     ]
 
 update : Msg -> Model -> Model
-update _ model = model
+update msg model =
+  case msg of
+    Paint point -> { model | currentPoint = point }
+
 
 main : Program () Model Msg
 main =
