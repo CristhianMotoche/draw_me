92fdbf2fc5ad1f3b7ce0669d3949a4020ca60f4a
CristhianMotoche <cristhian.motoche@gmail.com>
2021-05-30 22:36:48 -0500

    Use elm canvas

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,10 @@
-module Canvas exposing (..)
+module Main exposing (..)
 
 import Browser
+import Canvas as C
 import Tuple as T
 import Html as H
+import Html.Attributes as HA
 import Html.Events.Extra.Mouse as M
 
 type alias Model =
@@ -21,11 +23,17 @@ init = { currentPoint = { x = 0, y = 0 } }
 
 view : Model -> H.Html Msg
 view model =
-  H.div
-    [ M.onDown <| \ev -> Paint { x = T.first ev.clientPos, y = T.second ev.clientPos } ]
-    [ H.text <| "X" ++ String.fromFloat model.currentPoint.x
-    , H.text <| "Y" ++ String.fromFloat model.currentPoint.y
-    ]
+  C.toHtml (300, 300)
+       [ HA.style "border" "1px solid black"  ]
+       [ C.shapes [] [ C.rect (0, 0) 100 50 ] ]
+--    [ M.onDown <|
+--        \ev -> Paint { x = T.first ev.clientPos, y = T.second ev.clientPos },
+--      HA.width 200,
+--      HA.height 100
+--    ]
+--    [ H.text <| "X" ++ String.fromFloat model.currentPoint.x
+--    , H.text <| "Y" ++ String.fromFloat model.currentPoint.y
+--    ]
 
 update : Msg -> Model -> Model
 update msg model =
