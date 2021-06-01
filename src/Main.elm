7f840a66766b629d3b3ea2fd2d83f8c28ea1c198
CristhianMotoche <cristhian.motoche@gmail.com>
2021-05-31 20:52:37 -0500

    Finish simple canvas

--- src/Main.elm
+++ src/Main.elm
@@ -10,8 +10,14 @@ import Html.Events.Extra.Mouse as M
 import Debug
 import Color
 
+
+type PaintMode
+  = Enabled
+  | Disabled
+
 type alias Model =
   { currentPoint : Point
+  , mode : PaintMode
   }
 
 type alias Point =
@@ -19,31 +25,57 @@ type alias Point =
   , y: Float
   }
 
-type Msg = Paint Point
+type Msg
+  = Paint Point
+  | TogglePaintMode
+  | NoOp
 
 init : Model
-init = { currentPoint = { x = 0, y = 0 } }
+init =
+  { currentPoint = { x = 0, y = 0 }
+  , mode = Disabled
+  }
+
+mkPoint : (Float, Float) -> Point
+mkPoint (x, y) = { x = x, y = y }
+
+-- View
 
 view : Model -> H.Html Msg
-view { currentPoint } =
+view { currentPoint, mode } =
   H.div
     [ HA.style "display" "flex" ]
     [ C.toHtml
         (300, 300)
         [ M.onDown <|
-            \ev -> Paint { x = T.first ev.offsetPos, y = T.second ev.offsetPos },
+            \_ -> TogglePaintMode,
+          M.onUp <|
+            \_ -> TogglePaintMode,
+          M.onMove <|
+            \ev ->
+              if mode == Enabled
+                 then Paint (mkPoint ev.offsetPos)
+                 else NoOp,
           HA.style "border" "1px solid black"  ]
         [ C.shapes [ CS.fill (Color.rgb255 100 100 10) ] [ C.rect (currentPoint.x, currentPoint.y) 5 5 ] ]
     ]
-  --    [ H.text <| "X" ++ String.fromFloat model.currentPoint.x
-  --    , H.text <| "Y" ++ String.fromFloat model.currentPoint.y
-  --    ]
+
+-- Update
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
     Paint point -> { model | currentPoint = point }
+    TogglePaintMode -> { model | mode = toggle model.mode }
+    NoOp -> model
 
+toggle : PaintMode -> PaintMode
+toggle m =
+  case m of
+    Enabled -> Disabled
+    _ -> Enabled
+
+-- Main
 
 main : Program () Model Msg
 main =
